3ee17e03fdfcca8fc11043faf6de0d863b9ab7e3
vsnowsc <jolonte13@gmail.com>
2023-01-11 20:00:55 -0300

    :lipstick: feat: hero

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,16 @@
         <nav flex h="4.688rem" justify-between p="1.375rem" items-center un-border="#404545" b-b>
             <div flex="row" gap="0.781rem">
                 <p>logo</p>
-                <p font="poppins 700" text-1.563rem uppercase>frogdevs</p>
+                <p font="poppins 700" text="1.563rem" uppercase>frogdevs</p>
             </div>
-            <i class="ri-menu-line ri-2x"></i>
+            <i class="ri-menu-line ri-2x" text-green></i>
         </nav>
+        <!-- hero -->
+        <div pt px="2.063rem" items-center>
+            <div font="poppins" text="center" gap="0.625rem">
+                <h1 font="700" text="3.042rem" leading="3.75rem">Inovando o futuro</h1>
+                <p text="1.563rem" leading="2.813rem text-#404545">FrogDevs transforma ideias em realidade com talento e dedicação.</p>
+            </div>
+        </div>
     </header>
 </template>
