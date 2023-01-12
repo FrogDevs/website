82eba92cbf61e434c33dc2997b34ec5ddd3762f7
vsnowsc <jolonte13@gmail.com>
2023-01-11 21:43:26 -0300

    :sparkles: feat: Structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import MyHeader from './components/MyHeader.vue';
+import MyMain from './components/MyMain.vue'
 </script>
 <template>
     <MyHeader />
+    <MyMain />
 </template>
