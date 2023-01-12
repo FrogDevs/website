c6f19d1be8e1f2351db4b7f06e165bfee863e847
vsnowsc <jolonte13@gmail.com>
2023-01-11 21:44:03 -0300

    :lipstick: feat: Card

---
+++ src/components/TheCard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+    <div w="18.375rem" h="18.375rem" py="2.188rem" px="1.375rem" b un-border="#404545" rounded="0.781rem" font="poppins" items-center justify-center text-center gap="0.625rem">
+        <h2 text="2.604rem black">Aplicação</h2>
+        <p text="1.25rem #404545">Aplicação multiplataforma para o gerenciamento de estoque em 2 unidades físicas.</p>
+    </div>
+</template>
