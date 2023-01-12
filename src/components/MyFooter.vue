19533d47ebdb67ec48dab39c343c9c383e9cb3fa
vsnowsc <jolonte13@gmail.com>
2023-01-12 18:08:11 -0300

    :sparkles: feat: Footer

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+    <footer class="flex flex-col" items-center py="3.75rem" px="1.375rem" gap="1.563rem" un-border="#404545" b-t>
+        <div items-center gap="0.781rem">
+            <div flex="row" gap="0.781rem">
+                <p>logo</p>
+                <p font="poppins 700" text="1.563rem" uppercase>frogdevs</p>
+            </div>
+            <p font="poppins" text="1.25rem center #404545">© {ano} Frogdevs, Inc. Todos os direitos reservados.</p>
+        </div>
+        <p font="poppins" text="1.25rem center #404545">TCC em Análise e Desenvolvimento de Sistemas - ETEC Jardim Ângela.</p>
+    </footer>
+</template>
