58a00b372cd027a68188285d630d5b16a6f3061c
vsnowsc <jolonte13@gmail.com>
2023-01-11 21:46:54 -0300

    :sparkles: feat: Main

---
+++ src/components/MyMain.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheCard from './TheCard.vue'
+import MemberCard from './MemberCard.vue'
+</script>
+<template>
+    <main flex="col" px="2.063rem" pt="1.875rem">
+        <!-- cards -->
+        <div gap="2.063rem">
+            <TheCard />
+            <TheCard />
+        </div>
+    </main>
+</template>
