4350c5a17762711bb9900507f90d45365704d15c
vsnowsc <jolonte13@gmail.com>
2023-01-11 21:44:25 -0300

    :lipstick: feat: Member Card

---
+++ src/components/MemberCard.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+    <div items-center gap="0.625rem">
+        <div w="12.25rem" h="12.25rem" b rounded="1.563rem black"></div>
+        <div font="poppins" text="1.25rem">
+            <p text-black>Helena Costa</p>
+            <p text-blue>Desing</p>
+        </div>
+    </div>
+</template>
