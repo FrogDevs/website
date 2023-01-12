a04372e73836d43cb0dad5bb8c0f36120c166de1
vsnowsc <jolonte13@gmail.com>
2023-01-11 21:47:55 -0300

    :lipstick: feat: client

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -9,5 +9,12 @@ import MemberCard from './MemberCard.vue'
             <TheCard />
             <TheCard />
         </div>
+        <!-- cliente -->
+        <div my="3.125rem" gap="0.781rem" text-center>
+            <p font="poppins" text="1.563rem #404545" leading="2.813rem">Serviços prestados a empresa</p>
+            <h2 font="ptserif 700" text="3.125rem black" leading="3.75rem">Equilíbrio Natural</h2>
+            <!-- background gradient -->
+            <hr mt="1.563rem" h="0.313rem" b-none bg-green>
+        </div>
     </main>
 </template>
