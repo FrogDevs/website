4ac26560d03bc644db9524f6d0a36dee4108b635
vsnowsc <jolonte13@gmail.com>
2023-01-14 14:35:28 -0300

    :lipstick: feat: Desing

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 <template>
-    <header flex="col">
+    <header bg-light>
         <nav flex h="4.688rem" justify-between p="1.375rem" items-center un-border="#404545" b-b>
-            <div flex="row" gap="0.781rem">
+            <div flex="row" items-center gap="0.781rem">
                 <p>logo</p>
                 <p font="poppins 700" text="1.563rem" uppercase>frogdevs</p>
             </div>
