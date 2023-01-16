723bd3e28036ac3fd841a498e20d04e7a68d90c2
vsnowsc <jolonte13@gmail.com>
2023-01-16 14:22:07 -0300

    :lipstick: feat: Styled card

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 <template>
-    <div w="18.375rem" h="18.375rem" py="2.188rem" px="1.375rem" b un-border="#404545" rounded="0.781rem" font="poppins" items-center justify-center text-center gap="0.625rem">
+    <div class="card2" w="18.375rem" h="18.375rem" py="2.188rem" px="1.375rem" b un-border="#404545" rounded="0.781rem" font="poppins" items-center justify-center text-center gap="0.625rem">
         <h2 text="2.604rem black">Aplicação</h2>
-        <p text="1.25rem #404545">Aplicação multiplataforma para o gerenciamento de estoque em 2 unidades físicas.</p>
+        <p text="1.25rem #404545">Aplicação multiplataforma para gerenciamento de estoque em 2 unidades físicas.</p>
     </div>
 </template>
+<style scoped>
+    .card1 {
+        background: rgb(255,247,248);
+        background: linear-gradient(320deg, rgba(255,247,248,1) 25%, rgba(96,239,255,0.15) 50%, rgba(0,255,135,0.5) 75%); 
+    }
+    .card2 {
+        background: rgb(255,247,248);
+        background: linear-gradient(140deg, rgba(255,247,248,1) 25%, rgba(0,255,135,0.05) 40%, rgba(96,239,255,0.5) 65%); 
+    }
+</style>
