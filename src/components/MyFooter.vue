dfaedc7beab59e9f8838443c754c97c600dcd3cc
vsnowsc <jolonte13@gmail.com>
2023-01-16 20:40:20 -0300

    :recycle: refactor

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -5,7 +5,7 @@ const year = date.getFullYear();
 
 </script>
 <template>
-    <footer class="flex flex-col" items-center py="3.75rem" px="1.375rem" gap="1.563rem" un-border="#404545" b-t>
+    <footer flex flex-col items-center py="3.75rem" px="1.375rem" gap="1.563rem" un-border="#404545" b-t>
         <div items-center gap="0.781rem">
             <div flex="row" items-center gap="0.781rem">
                 <img src="src/assets/logo.svg" alt="logo">
